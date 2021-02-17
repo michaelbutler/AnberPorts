38eb09dd6d1d6de666a09b86bb02294f6abee144
krishenriksen <info@krishenriksen.dk>
2021-02-17 23:43:27 +0700

    refactor

--- AnberPorts-Keyboard-Mouse/main.c
+++ AnberPorts-Keyboard-Mouse/main.c
@@ -1,3 +1,25 @@
+/* Copyright (c) 2021
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+#
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+#
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the
+* Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301 USA
+#
+* Authored by: Kris Henriksen <krishenriksen.work@gmail.com>
+#
+* AnberPorts-Keyboard-Mouse
+*/
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,10 +39,8 @@ int fd_ev_joypad;
 int rc_joypad;
 struct input_event ev_joypad;
 
-/* Set up a fake keyboard device */
 static int uinp_fd = -1;
 struct uinput_user_dev uidev;
-struct input_event ev;
 
 #define select 311
 #define start 310
@@ -31,63 +51,84 @@ struct input_event ev;
 #define left 16
 #define right 16
 
-void send_key(int key) {
-	// Report BUTTON CLICK - PRESS event
-	memset(&ev, 0, sizeof(ev));
-	gettimeofday(&ev.time, NULL);
-	ev.type = EV_KEY;
-	ev.code = key; 
-	ev.value = 1;
-	write(uinp_fd, &ev, sizeof(ev));
+void emit(int type, int code, int val) {
+   struct input_event ev;
 
-	ev.type = EV_SYN;
-	ev.code = SYN_REPORT;
-	ev.value = 0;
-	write(uinp_fd, &ev, sizeof(ev));
+   ev.type = type;
+   ev.code = code;
+   ev.value = val;
+   /* timestamp values below are ignored */
+   ev.time.tv_sec = 0;
+   ev.time.tv_usec = 0;
 
-	// Report BUTTON CLICK - RELEASE event
-	memset(&ev, 0, sizeof(ev));
-	gettimeofday(&ev.time, NULL);
-	ev.type = EV_KEY;
-	ev.code = key;
-	ev.value = 0;
-	write(uinp_fd, &ev, sizeof(ev));
-
-	ev.type = EV_SYN;
-	ev.code = SYN_REPORT;
-	ev.value = 0;
-	write(uinp_fd, &ev, sizeof(ev));
+   write(uinp_fd, &ev, sizeof(ev));
 }
 
 void handle_event(int type, int code, int value) {
 	if (type == 1) {
 		if (code == start && (value == 1 || value == 2)) {
-			send_key(KEY_ENTER);
+			emit(EV_KEY, KEY_ENTER, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
 		}
 		else if (code == select && (value == 1 || value == 2)) {
-			send_key(KEY_ESC);
+			emit(EV_KEY, KEY_ESC, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
 		}
-		else if (code == a && (value == 1 || value == 2)) { // crouch
-			send_key(KEY_ENTER);
-		}		
-		else if (code == b && (value == 1 || value == 2)) { // jump
-			send_key(KEY_ESC);
+		else if (code == a && (value == 1 || value == 2)) {
+			emit(EV_KEY, KEY_ENTER, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
+		}
+		else if (code == b && (value == 1 || value == 2)) {
+			emit(EV_KEY, KEY_ESC, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
+		}
+		else {
+			// reset press down
+			emit(EV_KEY, KEY_ENTER, 0);
+			emit(EV_SYN, SYN_REPORT, 0);
+
+			emit(EV_KEY, KEY_ESC, 0);
+			emit(EV_SYN, SYN_REPORT, 0);
 		}
 	}
 
 	// d-pad
 	if (type == 3) {
 		if (code == up && value == -1) {
-			send_key(KEY_UP);
+			emit(EV_KEY, KEY_UP, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
 		}
-		else if (code == down && value == 1) {
-			send_key(KEY_DOWN);	
+		else if (code == up && value == 0) {
+			emit(EV_KEY, KEY_UP, 0);
+			emit(EV_SYN, SYN_REPORT, 0);
 		}
-		else if (code == left && value == -1) {
-			send_key(KEY_LEFT);
+
+		if (code == down && value == 1) {
+			emit(EV_KEY, KEY_DOWN, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
+
 		}
-		else if (code == right && value == 1) {
-			send_key(KEY_RIGHT);
+		else if (code == down && value == 0) {
+			emit(EV_KEY, KEY_DOWN, 0);
+			emit(EV_SYN, SYN_REPORT, 0);
+		}
+
+		if (code == left && value == -1) {
+			emit(EV_KEY, KEY_LEFT, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
+		}
+		else if (code == left && value == 0) {
+			emit(EV_KEY, KEY_LEFT, 0);
+			emit(EV_SYN, SYN_REPORT, 0);
+		}
+
+		if (code == right && value == 1) {
+			emit(EV_KEY, KEY_RIGHT, 1);
+			emit(EV_SYN, SYN_REPORT, 0);
+		}
+		else if (code == right && value == 0) {
+			emit(EV_KEY, KEY_RIGHT, 0);
+			emit(EV_SYN, SYN_REPORT, 0);
 		}
 	}
 }
@@ -105,8 +146,10 @@ int main () {
 	memset(&uidev, 0, sizeof(uidev));
 
 	strncpy(uidev.name, "AnberPorts Keyboard", UINPUT_MAX_NAME_SIZE);
-	uidev.id.version = 4;
+	uidev.id.version = 1;
 	uidev.id.bustype = BUS_USB;
+	uidev.id.vendor = 0x1234; /* sample vendor */
+	uidev.id.product = 0x5678; /* sample product */
 
 	for (i = 0; i < 256; i++) {
 		ioctl(uinp_fd, UI_SET_KEYBIT, i);
@@ -133,11 +176,17 @@ int main () {
 			handle_event(ev_joypad.type, ev_joypad.code, ev_joypad.value);
 		}
 
-		usleep(500);
+		usleep(100);
 	} while (rc_joypad == LIBEVDEV_READ_STATUS_SYNC || rc_joypad == LIBEVDEV_READ_STATUS_SUCCESS || rc_joypad == -EAGAIN);
 
 	libevdev_free(dev_joypad);
 	close(fd_ev_joypad);
+
+	/*
+	* Give userspace some time to read the events before we destroy the
+	* device with UI_DEV_DESTROY.
+	*/
+	sleep(1);
 
 	/* Clean up */
 	ioctl(uinp_fd, UI_DEV_DESTROY);
